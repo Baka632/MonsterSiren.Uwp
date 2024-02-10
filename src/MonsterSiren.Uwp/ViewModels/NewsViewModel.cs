c1e8a8e7df2c63d354d2f48bb7c902dbfcb16f37
Baka632 <stevemc123456@outlook.com>
2024-02-10 18:07:18 +0800

    添加初步的新闻列表查看功能

--- src/MonsterSiren.Uwp/ViewModels/NewsViewModel.cs
+++ src/MonsterSiren.Uwp/ViewModels/NewsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading;
 
 namespace MonsterSiren.Uwp.ViewModels;
 
@@ -12,16 +14,19 @@ public sealed partial class NewsViewModel : ObservableObject
     private ErrorInfo errorInfo;
     [ObservableProperty]
     private IList<RecommendedNewsInfo> recommendedNewsInfos;
+    [ObservableProperty]
+    private NewsItemCollection newsInfos;
 
     public async Task Initialize()
     {
         IsLoading = true;
         ErrorVisibility = Visibility.Collapsed;
 
-
         try
         {
             RecommendedNewsInfos = (await NewsService.GetRecommendedNewsAsync()).ToList();
+            NewsInfos = new NewsItemCollection(await NewsService.GetNewsListAsync());
+
             ErrorVisibility = Visibility.Collapsed;
         }
         catch (HttpRequestException ex)
@@ -44,4 +49,68 @@ public sealed partial class NewsViewModel : ObservableObject
             Exception = ex
         };
     }
+}
+
+public sealed class NewsItemCollection : ObservableCollection<NewsInfo>, ISupportIncrementalLoading
+{
+    private string lastCid = null;
+
+    public event Action<Exception> ErrorOccured;
+
+    public NewsItemCollection(ListPackage<NewsInfo> newsList) : base(newsList.List)
+    {
+        if (newsList.IsEnd == true)
+        {
+            HasMoreItems = false;
+        }
+        else
+        {
+            HasMoreItems = true;
+            lastCid = newsList.List.Last().Cid;
+        }
+    }
+
+    public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+    {
+        return AsyncInfo.Run(LoadFromServer);
+    }
+
+    private async Task<LoadMoreItemsResult> LoadFromServer(CancellationToken token)
+    {
+        LoadMoreItemsResult result = new();
+
+        try
+        {
+            ListPackage<NewsInfo> newsList = string.IsNullOrWhiteSpace(lastCid)
+                ? await Task.Run(NewsService.GetNewsListAsync, token)
+                : await Task.Run(() => NewsService.GetNewsListAsync(lastCid), token);
+
+            uint count = 0;
+            foreach (NewsInfo item in newsList.List)
+            {
+                Add(item);
+                count++;
+            }
+
+            result.Count = count;
+
+            if (newsList.IsEnd == true)
+            {
+                HasMoreItems = false;
+            }
+            else
+            {
+                HasMoreItems = true;
+                lastCid = newsList.List.Last().Cid;
+            }
+        }
+        catch (Exception ex)
+        {
+            ErrorOccured?.Invoke(ex);
+        }
+
+        return result;
+    }
+
+    public bool HasMoreItems { get; private set; }
 }
