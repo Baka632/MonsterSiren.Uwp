90b244affef306d4b1400b0ff625cddfe7ab26e6
Baka632 <stevemc123456@outlook.com>
2024-02-13 22:44:44 +0800

    调整在移动端的文本大小和图像宽度

--- src/MonsterSiren.Uwp/ViewModels/NewsDetailViewModel.cs
+++ src/MonsterSiren.Uwp/ViewModels/NewsDetailViewModel.cs
@@ -29,10 +29,24 @@ public sealed partial class NewsDetailViewModel : ObservableObject
                 $"document.getElementById('mainContent').style.color = 'rgb({textColor.R}, {textColor.G}, {textColor.B})'",
             });
 
+            // 设置文本大小
+            string fontSizeString = EnvironmentHelper.IsWindowsMobile ? "2.3rem" : "14px";
+            await targetWebView.InvokeScriptAsync("eval", new[]
+            {
+                $"document.styleSheets[0].insertRule('p {{ font-size: {fontSizeString}; }}', 0);"
+            });
+
             // 设置被选中文本背景色
             await targetWebView.InvokeScriptAsync("eval", new[]
             {
                 $"document.styleSheets[0].insertRule('p::selection {{ background: rgb({accentColor.R}, {accentColor.G}, {accentColor.B}) }}', 0);"
+            });
+
+            // 设置图像宽度
+            string widthPercentage = EnvironmentHelper.IsWindowsMobile ? "90%" : "70%";
+            await targetWebView.InvokeScriptAsync("eval", new[]
+            {
+                $"document.styleSheets[0].insertRule('img {{ max-width: {widthPercentage}; }}', 0);"
             });
 
             targetWebView.NavigationStarting += async (webView, args) =>
