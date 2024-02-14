008f11fb25e0a8e1767f7f956d3770f49456a428
Baka632 <stevemc123456@outlook.com>
2024-02-14 22:17:13 +0800

    添加搜索功能

---
+++ src/MonsterSiren.Uwp/Models/MsrIncrementalCollection.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace MonsterSiren.Uwp.Models;
+
+/// <summary>
+/// 使用塞壬唱片相关服务的增量集合
+/// </summary>
+/// <typeparam name="T">集合类型</typeparam>
+public sealed class MsrIncrementalCollection<T> : ObservableCollection<T>, ISupportIncrementalLoading
+{
+    private T lastObject = default;
+    private readonly Func<T, Task<ListPackage<T>>> loadMoreDelegate;
+
+    public event Action<Exception> ErrorOccured;
+
+    public MsrIncrementalCollection(ListPackage<T> listPkg, Func<T, Task<ListPackage<T>>> loadMoreFunc) : base(listPkg.List)
+    {
+        if (loadMoreFunc is null)
+        {
+            throw new ArgumentNullException(nameof(loadMoreFunc));
+        }
+        else
+        {
+            loadMoreDelegate = loadMoreFunc;
+        }
+
+        if (listPkg.IsEnd == true)
+        {
+            HasMoreItems = false;
+        }
+        else
+        {
+            HasMoreItems = true;
+            lastObject = listPkg.List.Last();
+        }
+    }
+
+    public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+    {
+        return AsyncInfo.Run(LoadFromServer);
+    }
+
+    private async Task<LoadMoreItemsResult> LoadFromServer(CancellationToken token)
+    {
+        LoadMoreItemsResult result = new();
+
+        try
+        {
+            ListPackage<T> listPkg = EqualityComparer<T>.Default.Equals(lastObject, default)
+                ? await Task.Run(() => loadMoreDelegate(default), token)
+                : await Task.Run(() => loadMoreDelegate(lastObject), token);
+
+            uint count = 0;
+            foreach (T item in listPkg.List)
+            {
+                Add(item);
+                count++;
+            }
+
+            result.Count = count;
+
+            if (listPkg.IsEnd == true)
+            {
+                HasMoreItems = false;
+            }
+            else
+            {
+                HasMoreItems = true;
+                lastObject = listPkg.List.Last();
+            }
+        }
+        catch (Exception ex)
+        {
+            ErrorOccured?.Invoke(ex);
+        }
+
+        return result;
+    }
+
+    public bool HasMoreItems { get; private set; }
+}
---
+++ src/MonsterSiren.Uwp/ViewModels/SearchViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.Http;
+
+namespace MonsterSiren.Uwp.ViewModels;
+
+public sealed partial class SearchViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool isLoading = false;
+    [ObservableProperty]
+    private Visibility errorVisibility = Visibility.Collapsed;
+    [ObservableProperty]
+    private ErrorInfo errorInfo;
+    [ObservableProperty]
+    private MsrIncrementalCollection<NewsInfo> _newsList;
+    [ObservableProperty]
+    private MsrIncrementalCollection<AlbumInfo> _albumList;
+    [ObservableProperty]
+    private bool isAlbumListEmpty;
+    [ObservableProperty]
+    private bool isNewsListEmpty;
+
+    partial void OnAlbumListChanged(MsrIncrementalCollection<AlbumInfo> value)
+    {
+        IsAlbumListEmpty = !value.Any();
+    }
+
+    partial void OnNewsListChanged(MsrIncrementalCollection<NewsInfo> value)
+    {
+        IsNewsListEmpty = !value.Any();
+    }
+
+    public async Task Initialize(string keyword)
+    {
+        IsLoading = true;
+        ErrorVisibility = Visibility.Collapsed;
+
+        try
+        {
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                return;
+            }
+
+            SearchAlbumAndNewsResult albumAndNewsWarpper = await SearchService.SearchAlbumAndNewsAsync(keyword);
+
+            NewsList = new MsrIncrementalCollection<NewsInfo>(albumAndNewsWarpper.News, async lastInfo => await SearchService.SearchNewsAsync(keyword, lastInfo.Cid));
+            AlbumList = new MsrIncrementalCollection<AlbumInfo>(albumAndNewsWarpper.Albums, async lastInfo => await SearchService.SearchAlbumAsync(keyword, lastInfo.Cid));
+
+            ErrorVisibility = Visibility.Collapsed;
+        }
+        catch (HttpRequestException ex)
+        {
+            ShowInternetError(ex);
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+
+    private void ShowInternetError(HttpRequestException ex)
+    {
+        ErrorVisibility = Visibility.Visible;
+        ErrorInfo = new ErrorInfo()
+        {
+            Title = "ErrorOccurred".GetLocalized(),
+            Message = "InternetErrorMessage".GetLocalized(),
+            Exception = ex
+        };
+    }
+
+    public static async Task DisplayContentDialog(string title, string message, string primaryButtonText = "", string closeButtonText = "")
+    {
+        ContentDialog contentDialog = new()
+        {
+            Title = title,
+            Content = message,
+            PrimaryButtonText = primaryButtonText,
+            CloseButtonText = closeButtonText
+        };
+
+        await contentDialog.ShowAsync();
+    }
+}
