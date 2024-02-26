39687625466e651ee5d7e78a9a65e2b19f0efe65
Baka632 <stevemc123456@outlook.com>
2024-02-27 01:23:31 +0800

    初步添加实现概览模式的代码

---
+++ src/MonsterSiren.Uwp/ViewModels/GlanceViewViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MonsterSiren.Uwp.ViewModels;
+
+public sealed class GlanceViewViewModel : ObservableObject
+{
+    private readonly DispatcherTimer _timer = new();
+
+    public MusicInfoService MusicInfo { get; } = MusicInfoService.Default;
+}
---
+++ src/MonsterSiren.Uwp/Views/GlanceViewPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+
+using Windows.UI.ViewManagement;
+
+namespace MonsterSiren.Uwp.Views;
+
+/// <summary>
+/// 可用于自身或导航至 Frame 内部的空白页。
+/// </summary>
+public sealed partial class GlanceViewPage : Page
+{
+    public GlanceViewViewModel ViewModel { get; } = new GlanceViewViewModel();
+
+    public GlanceViewPage()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        base.OnNavigatedTo(e);
+
+        ApplicationView view = ApplicationView.GetForCurrentView();
+        if (view.IsFullScreenMode != true)
+        {
+            view.TryEnterFullScreenMode();
+        }
+    }
+
+    protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+    {
+        base.OnNavigatingFrom(e);
+
+        ApplicationView view = ApplicationView.GetForCurrentView();
+        if (view.IsFullScreenMode)
+        {
+            view.ExitFullScreenMode();
+        }
+    }
+
+    private void OnDoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+    {
+        Frame.GoBack();
+    }
+}
