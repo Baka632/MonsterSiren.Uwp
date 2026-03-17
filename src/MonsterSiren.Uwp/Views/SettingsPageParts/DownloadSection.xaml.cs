893f1b3db6dde5d2d6f376c85d218253d5cea8b8
Baka632 <stevemc123456@outlook.com>
2026-03-17 22:48:58 +0800

    将 SettingsPage 中的内容拆分为用户控件，以简化 XAML 文件。

---
+++ src/MonsterSiren.Uwp/Views/SettingsPageParts/DownloadSection.xaml.cs
@@ -0,0 +1,18 @@
+namespace MonsterSiren.Uwp.Views.SettingsPageParts;
+
+public sealed partial class DownloadSection : UserControl
+{
+    public SettingsViewModel ViewModel
+    {
+        get => (SettingsViewModel)GetValue(ViewModelProperty);
+        set => SetValue(ViewModelProperty, value);
+    }
+
+    public static readonly DependencyProperty ViewModelProperty =
+        DependencyProperty.Register(nameof(ViewModel), typeof(SettingsViewModel), typeof(PresentationSection), new PropertyMetadata(null));
+
+    public DownloadSection()
+    {
+        this.InitializeComponent();
+    }
+}
